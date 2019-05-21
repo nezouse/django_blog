7068a7120dc5c0bf14b97bc89f44e78caba6d4fd
Nezouse <stankiewicz1998@gmail.com>
2019-05-21 17:33:31 +0200

    Implement search results rank, title have bigger weight

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count
@@ -99,8 +99,10 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
             results = Post.objects.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                rank=SearchRank(search_vector, search_query)
+            ).filter(rank__gte=0.3).order_by('-rank')
     return render(request, 'blog/post/search.html',
                   {'form': form, 'query': query, 'results': results})
