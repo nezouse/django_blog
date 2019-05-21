854fc70e7fb59e8d8a2c5660abaec290bd1aa821
Nezouse <stankiewicz1998@gmail.com>
2019-05-21 13:36:25 +0200

    Create template to share a post

--- blog/views.py
+++ blog/views.py
@@ -33,7 +33,7 @@ def post_share(request, post_id):
             post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = f'{cd["name"]} ({cd["email"]}) recommends you reading {post.title}'
             message = f'Read "{post.title}" at {post_url}\n\n{cd["name"]}\'s comments: {cd["comments"]}'
-            send_mail(subject, message, 'admin@myblog.com')
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
             sent = True
     else:
         form = EmailPostForm()
